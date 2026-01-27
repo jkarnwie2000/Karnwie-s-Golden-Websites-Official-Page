f971c91b15befa4d006dffbd5b87a7bcb3db265a
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-01-27 14:25:17 -0500

    test run

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 
+let contrastToggle = false
 
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+    document.body.classList += " dark-theme"
+}
+else {
+    document.body.classList.remove("dark-theme")
+}
+}
 
 function contact(event) {
     event.preventDefault();
@@ -23,11 +33,11 @@ function contact(event) {
 
 let isModalOpen = false
 function toggleModal() {
-isModalOpen = !isModalOpen
 if (isModalOpen) {
+    isModalOpen = false
     return document.body.classList.remove("modal--open")
 }
-
+isModalOpen = true
 document.body.classList += " modal--open"
 }
 
